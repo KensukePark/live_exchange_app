edc93b771355ebc1f7d0d6b45e77e56e6d883b55
HyunWoo <iu4148@gmail.com>
2023-10-16 01:13:50 +0900

    오프라인 기능 추가 2023-10-16

---
+++ lib/screens/rate_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RatePage extends StatefulWidget {
+  const RatePage({Key? key}) : super(key: key);
+
+  @override
+  State<RatePage> createState() => _RatePageState();
+}
+
+class _RatePageState extends State<RatePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
