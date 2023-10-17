6abf766e3eb1eb82065402b8f89455c2287ddb1a
HyunWoo <iu4148@gmail.com>
2023-10-17 23:47:42 +0900

    사용자 지정 환율 기능 추가 2023-10-17

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -21,13 +21,27 @@ class _LoadingState extends State<Loading> {
   var now = DateTime.now();
   String formatDate = '';
   late bool check;
-
+  late bool check_custom;
+  late List<String> temp_list;
   @override
+  /*
+  check_bool 함수:
+  로컬 스토리지에 저장된 bool값들을 체크하는 함수
+  */
   void check_bool() async {
     var prefs = await SharedPreferences.getInstance();
+    //prefs.clear();
     if (prefs.containsKey('bool') == false) {
       prefs.setBool('bool', false);
     }
+    if (prefs.containsKey('custom_mode') == false) {
+      prefs.setBool('custom_mode', false);
+    }
+    if (prefs.containsKey('user_rate') == false) {
+      prefs.setStringList('user_rate', ['1', '200', '1200', '10']);
+    }
+    check_custom = prefs.getBool('custom_mode')!;
+    temp_list = prefs.getStringList('user_rate')!;
     if (prefs.getBool('bool') == false) {
       getprice();
       check = false;
@@ -60,31 +74,48 @@ class _LoadingState extends State<Loading> {
   }
 
   /*
-  오프라인에서 환율 테스트용 함수
+  getprice_offline 함수:
+  오프라인 모드로 환율 데이터를 갖고오는 함수
    */
   void getprice_offline() {
+    //개발자가 설정한 환율 데이터값읗 환율 데이터로 사용
+    if (check_custom == false) {
+      cur_rate.add(1);
+      cur_rate.add(180);
+      cur_rate.add(1200);
+      cur_rate.add(9.5);
+
+    }
+    //사용자 지정 환율 데이터값을 사용
+    else {
+      for (int i=0; i<4; i++) {
+        cur_rate.add(num.parse(temp_list![i]));
+      }
+    }
     cur_list.add('KRW');
-    cur_rate.add(1);
     cur_list.add('CNY');
-    cur_rate.add(184);
     cur_list.add('USD');
-    cur_rate.add(1170);
     cur_list.add('JPY');
-    cur_rate.add(9.5);
     print('***오프라인***');
     print(cur_list);
     print(cur_rate);
     Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context){
-      return HomePage(cur_name: cur_list, cur_rate: cur_rate, date: formatDate, check: check,);
+      return HomePage(cur_name: cur_list, cur_rate: cur_rate, date: formatDate, check: check, mode_check: check_custom);
     }), (route) => false);
   }
 
+  /*
+  getprice 함수:
+  실시간으로 최신 환율 데이터값을 받아와 사용
+   */
   void getprice() async {
     int cnt = 0;
     Network network = Network('https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=' + api_keys + '&searchdate=' + formatDate + '&data=AP01');
     var Data = await network.getData();
+    print('getData 이후 확인');
     //if (Data.length == 0) print('data is null');
     while (Data.length == 0) {
+      print('while 문 확인');
       now = now.subtract(Duration(days:1));
       formatDate = DateFormat('yyyyMMdd').format(now);
       network = Network('https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=' + api_keys + '&searchdate=' + formatDate + '&data=AP01');
@@ -93,7 +124,11 @@ class _LoadingState extends State<Loading> {
       cnt++;
       if (cnt == 10) break;
     }
-
+    if (Data.length == 0) {
+      print('인터넷 연결 오류');
+      print('인터넷 연결 오류');
+      print('인터넷 연결 오류');
+    }
     for (int i=0; i<Data.length; i++) {
       if (Data[i]['cur_unit'] == 'CNH') {
         cur_list.add('CNY');
@@ -116,9 +151,8 @@ class _LoadingState extends State<Loading> {
     }
     print(cur_list);
     print(cur_rate);
-
     Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context){
-      return HomePage(cur_name: cur_list, cur_rate: cur_rate, date: formatDate, check: check,);
+      return HomePage(cur_name: cur_list, cur_rate: cur_rate, date: formatDate, check: check, mode_check: check_custom);
     }), (route) => false);
   }
 
