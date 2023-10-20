b360cb81a06230540b117c2760870846ec772ad2
HyunWoo <iu4148@gmail.com>
2023-10-21 01:00:53 +0900

    2023.10.21

--- lib/screens/rate_page.dart
+++ lib/screens/rate_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class RatePage extends StatefulWidget {
-  const RatePage({Key? key}) : super(key: key);
+  const RatePage({Key? key, required this.result, required this.img, required this.date}) : super(key: key);
+  final List<String> result;
+  final List<String> img;
+  final String date;
 
   @override
   State<RatePage> createState() => _RatePageState();
@@ -11,218 +14,124 @@ class _RatePageState extends State<RatePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.black),
-        title: Text(
-          '은행 고시환율',
-          style: TextStyle(
-              color: Colors.black
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.black),
+          title: Text(
+            '은행 고시환율',
+            style: TextStyle(
+                color: Colors.black
+            ),
           ),
+          backgroundColor: Color(0xffFEFAF8),
+          centerTitle: true,
         ),
-        backgroundColor: Color(0xffFEFAF8),
-        centerTitle: true,
-      ),
-      body: SafeArea(
-        child: Container(
-          color: Colors.white,
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: SafeArea(
+            child: Container(
+              color: Colors.white,
+              child: Column(
                 children: [
-                  Container(
-                    padding: EdgeInsets.fromLTRB(10,20,5,5),
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          border: Border.all(
-                            color: Colors.grey.shade400,
-                            width: 1,
-                          )
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('미국 USD'),
-                          SizedBox(height: 5,),
-                          Text(
-                            '1,359',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold
-                            ),
-                          ),
-                          Image.network(
-                              'https://ssl.pstatic.net/imgfinance/chart/mobile/marketindex/month3/FX_USDKRW_SHB_search.png?sidcode=1697714653445',),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(5,20,10,5),
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child: Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5),
-                            border: Border.all(
-                              color: Colors.grey.shade400,
-                              width: 1,
-                            )
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                  ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: widget.img.length,
+                    itemBuilder: (BuildContext context, int idx) {
+                      return Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
                           children: [
-                            Text('일본 JPY 100'),
-                            SizedBox(height: 5,),
-                            Text(
-                              '907',
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold
+                            Container(
+                              padding: EdgeInsets.fromLTRB(10,10,10,2.5),
+                              width: MediaQuery.of(context).size.width,
+                              child: Container(
+                                padding: EdgeInsets.all(10),
+                                child: Row(
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          widget.result[5*idx],
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold
+                                          ),
+                                        ),
+                                        Text(
+                                          widget.result[5*idx+1],
+                                          style: TextStyle(
+                                              fontSize: 16,
+                                              color: Colors.grey
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Expanded(
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.end,
+                                          children: [
+                                            Image.network(
+                                              widget.img[idx],
+                                              height: MediaQuery.of(context).size.height * 0.065,
+                                            ),
+                                            Container(
+                                              width: MediaQuery.of(context).size.width * 0.18,
+                                              child: Column(
+                                                crossAxisAlignment: CrossAxisAlignment.end,
+                                                children: [
+                                                  Text(
+                                                    //widget.result[5*idx+2].contains('.') ? widget.result[5*idx+2].substring(0,widget.result[5*idx+2].length-3) :
+                                                    widget.result[5*idx+2],
+                                                    style: TextStyle(
+                                                        fontSize: 16,
+                                                        fontWeight: FontWeight.bold
+                                                    ),
+                                                  ),
+                                                  Container(
+                                                    padding: EdgeInsets.fromLTRB(5,2,5,2),
+                                                    decoration: BoxDecoration(
+                                                      color: widget.result[5*idx+4].contains('-') ? Colors.red : Colors.greenAccent,
+                                                      borderRadius: BorderRadius.circular(5),
+                                                    ),
+                                                    child: Text(
+                                                      widget.result[5*idx+4],
+                                                      style: TextStyle(
+                                                          color: Colors.white
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ],
+                                        )
+                                    )
+                                  ],
+                                ),
                               ),
                             ),
-                            Image.network('https://ssl.pstatic.net/imgfinance/chart/mobile/marketindex/month3/FX_JPYKRW_SHB_search.png?sidcode=1697714653445'),
                           ],
-                        )
-                    ),
+                        ),
+                      );
+                    }
                   ),
+                  Expanded(
+                      child: Align(
+                        alignment: Alignment.bottomRight,
+                        child: Container(
+                          padding: EdgeInsets.all(15),
+                          child: Text(
+                            widget.date + ' 기준 환율',
+                            style: TextStyle(
+                                color: Colors.grey
+                            ),
+                          ),
+                        ),
+                      )
+                  )
                 ],
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                      padding: EdgeInsets.fromLTRB(10,5,5,5),
-                      width: MediaQuery.of(context).size.width * 0.5 ,
-                      child: Container(
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              border: Border.all(
-                                color: Colors.grey.shade400,
-                                width: 1,
-                              )
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('유럽 EUR'),
-                              SizedBox(height: 5,),
-                              Text(
-                                '1,436',
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold
-                                ),
-                              ),
-                              Image.network(
-                                  'https://ssl.pstatic.net/imgfinance/chart/mobile/marketindex/month3/FX_EURKRW_SHB_search.png?sidcode=1697714653445'
-                              ),
-                            ],
-                          )
-                      )
-                  ),
-                  Container(
-                      padding: EdgeInsets.fromLTRB(5,5,10,5),
-                      width: MediaQuery.of(context).size.width * 0.5 ,
-                      child: Container(
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              border: Border.all(
-                                color: Colors.grey.shade400,
-                                width: 1,
-                              )
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('중국 CNY'),
-                              SizedBox(height: 5,),
-                              Text(
-                                '185',
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold
-                                ),
-                              ),
-                              Image.network('https://ssl.pstatic.net/imgfinance/chart/mobile/marketindex/month3/FX_CNYKRW_SHB_search.png?sidcode=1697714653445'),
-                            ],
-                          ))
-                  ),
-
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                      padding: EdgeInsets.fromLTRB(10,5,5,5),
-                      width: MediaQuery.of(context).size.width * 0.5,
-                      child: Container(
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              border: Border.all(
-                                color: Colors.grey.shade400,
-                                width: 1,
-                              )
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('영국 GBP'),
-                              SizedBox(height: 5,),
-                              Text(
-                                '1,650',
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold
-                                ),
-                              ),
-                              Image.network('https://ssl.pstatic.net/imgfinance/chart/mobile/marketindex/month3/FX_GBPKRW_SHB_search.png?sidcode=1697714653445'),
-                            ],
-                          )
-                      )
-                  ),
-                  Container(
-                      padding: EdgeInsets.fromLTRB(5,5,10,5),
-                      width: MediaQuery.of(context).size.width * 0.5,
-                      child: Container(
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              border: Border.all(
-                                color: Colors.grey.shade400,
-                                width: 1,
-                              )
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('호주 AUD'),
-                              SizedBox(height: 5,),
-                              Text(
-                                '858',
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold
-                                ),
-                              ),
-                              Image.network('https://ssl.pstatic.net/imgfinance/chart/mobile/marketindex/month3/FX_AUDKRW_SHB_search.png?sidcode=1697714653445'),
-                            ],
-                          )
-                      )
-                  ),
-                ],
-              ),
-            ],
-          ),
+            )
         )
-      )
     );
   }
 }
